7382e216e11301293ddd64a45b6c640de80cf81a
rgriff452 <rgriff@gmail.com>
2015-02-02 02:14:33 +0200

    InitialCommit

---
+++ shader_compiler/compile.bat
@@ -0,0 +1,5 @@
+2MGFX.exe PerlinNoise.fx PerlinNoise.2mgfx
+2MGFX.exe SkyDome.fx SkyDome.2mgfx
+2MGFX.exe SolidBlockEffect.fx SolidBlockEffect.2mgfx
+2MGFX.exe WaterBlockEffect.fx WaterBlockEffect.2mgfx
+pause
